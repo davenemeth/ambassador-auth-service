b43c135fcde85c046896741bd184dce521d3b7d2
David Nemeth <david.nemeth@robotron.de>
2019-01-10 09:44:46 +0100

    syntax fix

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
-rem set regitry and version
-set DOCKER_REGISTRY=cloud.canister.io
-set VERSION=1.0
+rem set registry and version
+set DOCKER_REGISTRY=cloud.canister.io:5000/davenemeth/ambassador-extauth-sample
+set VERSION=1.0.0
 
 echo using regitry: %DOCKER_REGISTRY%
 echo version: %VERSION%
@@ -11,9 +11,9 @@ rem docker login cloud.canister.io
 
 rem build image
 echo Starting to build docker image...
-docker build -t %DOCKER_REGISTRY%/sample-auth-service:%VERSION% .
+docker build -t %DOCKER_REGISTRY%:%VERSION% .
 
 rem push image
 echo Pushing image...
-docker push %DOCKER_REGISTRY%/sample-auth-service:%VERSION%
+docker push %DOCKER_REGISTRY%:%VERSION%
 
